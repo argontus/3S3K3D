2dea55bbe395b9bb64abd930ee738a752cf6a5d3
argontus <juhani.alanko@gmail.com>
2011-02-20 13:54:13 +0200

    Added project for sound module 	modified:   codeblocks/linux.workspace 	new file:   codeblocks/sound/sound.cbp 	new file:   codeblocks/sound/sound.layout 	new file:   codeblocks/sound/sound.layout.save 	new file:   include/sound/mixer.h 	new file:   src/sound/mixer.cpp

---
+++ include/sound/mixer.h
@@ -0,0 +1,21 @@
+/**
+ * @file sound/mixer.h
+ * @author Juhani Alanko
+ */
+
+#ifndef MIXER_H
+#define MIXER_H
+
+/**
+ * An interface class for sdl_mixer
+ */
+class Mixer
+{
+    public:
+        Mixer();
+        virtual ~Mixer();
+    protected:
+    private:
+};
+
+#endif // MIXER_H
---
+++ src/sound/mixer.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file sound/mixer.cpp
+ * @author Juhani Alanko
+ */
+
+#include "mixer.h"
+
+Mixer::Mixer()
+{
+    //ctor
+}
+
+Mixer::~Mixer()
+{
+    //dtor
+}
