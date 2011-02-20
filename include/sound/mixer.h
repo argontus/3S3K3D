54b8ba1b9ba9cfeb3f641e4ac5eb61b0cfa8bc08
argontus <juhani.alanko@gmail.com>
2011-02-21 00:38:31 +0200

    Basic functionality in sound engine

--- include/sound/mixer.h
+++ include/sound/mixer.h
@@ -6,16 +6,91 @@
 #ifndef MIXER_H
 #define MIXER_H
 
+#include <SDL/SDL_mixer.h>
+#include <iostream>
+#include <vector>
+
 /**
  * An interface class for sdl_mixer
  */
 class Mixer
 {
-    public:
-        Mixer();
-        virtual ~Mixer();
-    protected:
-    private:
+public:
+    /**
+     * Constructor.
+     */
+    Mixer();
+
+    /**
+     * Destructor.
+     */
+    virtual ~Mixer();
+
+    /**
+     * Initialises the audio engine with custom values
+     * @param frequency sampling frequency in samples per second
+     * @param format sample format
+     * @param channels number of output channels, 1 = mono, 2 = stereo etc.
+     * @param chunksize size of sample buffer
+     * @return bool true if succeeded, false otherwise
+     */
+    bool init( int frequency, Uint16 format, int channels, int chunksize );
+
+    /**
+     * Close the audio engine and clean up.
+     */
+    void close();
+
+    /**
+     * Loads an audio chunk, in other words a sound effect, into the audio engine.
+     * Supports multiple audio formats.
+     * Avoid using large files, use loadMusic for music.
+     * @param filename the name of the sound file
+     * @param chunkname a name for the sound effect, needed for actual playback
+     * @return int -1 if failed, otherwise the index of the added chunk (needed for playback later)
+     */
+    int loadChunk( std::string filename );
+
+    /**
+     * Loads a music file into the engine. Supports multiple audio formats.
+     * @param filename the name of the music file
+     * @param songname a name for the music piece, needed for actual playback
+     * @return int -1 if failed, otherwise the index of the added song (needed for playback later)
+     */
+    int loadMusic( std::string filename );
+
+    /**
+     * Plays a sound effect.
+     * @param id the id of the sound effect that is to be played, returned by loadMusic()
+     * @param loops the number of loops, -1 for infinite loop
+     * @return int the channel number assigned to playing the chunk, -1 on error
+     */
+    int playChunk( int id, int loops );
+
+    /**
+     * Plays music.
+     * @param id the id of the piece of music that is to be played, returned by loadChunk()
+     */
+    void playMusic( int id );
+
+    /**
+     * Stops music playback.
+     */
+    void stopMusic();
+
+    /**
+     * Tells if music is playing or not.
+     */
+    inline bool isMusicPlaying() {  return (Mix_PlayingMusic() ? true : false); }
+
+private:
+    /**
+     * Frees the loaded sounds and music
+     */
+    void freeSounds();
+
+    std::vector<Mix_Chunk*>     chunkList_;
+    std::vector<Mix_Music*>     musicList_;
 };
 
 #endif // MIXER_H
--- src/sound/mixer.cpp
+++ src/sound/mixer.cpp
@@ -3,14 +3,154 @@
  * @author Juhani Alanko
  */
 
-#include "mixer.h"
+#include "sound/mixer.h"
+
+#include <SDL/SDL.h>
+
 
 Mixer::Mixer()
 {
     //ctor
 }
 
+
 Mixer::~Mixer()
 {
     //dtor
+}
+
+
+bool Mixer::init( int frequency, Uint16 format, int channels, int chunksize )
+{
+    SDL_InitSubSystem( SDL_INIT_AUDIO );
+    // TODO: hardcoded for OGG, might be a good idea to change this
+    Mix_Init( MIX_INIT_OGG );
+
+    if( Mix_OpenAudio(  frequency,
+                        format,
+                        channels,
+                        chunksize) < 0 )
+    {
+        return false;
+    }
+
+    return true;
+}
+
+
+void Mixer::close()
+{
+    stopMusic();
+
+    freeSounds();
+    Mix_CloseAudio();
+    Mix_Quit();
+}
+
+
+int Mixer::loadChunk( std::string filename )
+{
+    Mix_Chunk* chunk = Mix_LoadWAV( (char*)filename.c_str() );
+    if( chunk == NULL )
+    {
+        return -1;
+    }
+
+    chunkList_.push_back(chunk);
+
+    // return the index of the loaded chunk
+    return ( chunkList_.size() - 1 );
+}
+
+
+int Mixer::loadMusic( std::string filename )
+{
+    Mix_Music* music = Mix_LoadMUS( (char*)filename.c_str() );
+    if( music == NULL )
+    {
+        return -1;
+    }
+
+    musicList_.push_back(music);
+
+    // return the index of the loaded music object
+    return ( musicList_.size() - 1);
+}
+
+
+int Mixer::playChunk( int id, int loops=0 )
+{
+    // sanity checks
+    if( id < 0 || id >= (int)chunkList_.size() )
+    {
+        return -1;
+    }
+
+    if( chunkList_[id] == NULL )
+    {
+        return -1;
+    }
+
+    return Mix_PlayChannel( -1, chunkList_[id], loops );
+}
+
+
+void Mixer::playMusic( int id )
+{
+    // sanity checks
+    if( id < 0 || id >= (int)musicList_.size() )
+    {
+        return;
+    }
+
+    if( musicList_[id] == NULL )
+    {
+        return;
+    }
+
+    Mix_Music* music = musicList_[id];
+
+    // check if there already is music playing
+    // if this is the case, fade out & in
+    // else just start playing
+    // TODO: real crossfading needs implementation of a multi-channel music mixer
+    if( Mix_PlayingMusic() )
+    {
+        Mix_FadeOutMusic( 500 );
+        while( Mix_FadingMusic() ) {}
+        Mix_FadeInMusic( music, -1, 500 );
+    }
+    else
+    {
+        Mix_PlayMusic( music, -1 );
+    }
+}
+
+
+void Mixer::stopMusic()
+{
+    if( Mix_PlayingMusic() )
+    {
+        Mix_HaltMusic();
+    }
+}
+
+
+void Mixer::freeSounds()
+{
+    // free chunks...
+    for ( int i=0; i < (int)chunkList_.size(); ++i )
+    {
+        Mix_FreeChunk( chunkList_[i] );
+    }
+
+    chunkList_.clear();
+
+    // ...and music
+    for ( int i=0; i < (int)musicList_.size(); ++i )
+    {
+        Mix_FreeMusic( musicList_[i] );
+    }
+
+    musicList_.clear();
 }
